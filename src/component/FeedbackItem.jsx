3a9bf3ca45ee0e79c86d2bed5c590011ff733ce1
Faiznhmd <fiaznhmd.0624@gmail.com>
2023-12-31 11:18:13 +0530

    implement usecontext and added click function using usecontext

--- src/component/FeedbackItem.jsx
+++ src/component/FeedbackItem.jsx
@@ -1,12 +1,18 @@
-import { FaTimes } from 'react-icons/Fa';
+import { FaTimes, FaEdit } from 'react-icons/Fa';
+import { useContext } from 'react';
+import FeedbackContext from '../context/FeedbackContext';
 import Card from './shared/Card';
 
-const FeedbackItem = ({ item, handleDelete }) => {
+const FeedbackItem = ({ item }) => {
+  const { deleteFeedback, editFeedback } = useContext(FeedbackContext);
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
-      <button className="close" onClick={() => handleDelete(item.id)}>
+      <button className="close" onClick={() => deleteFeedback(item.id)}>
         <FaTimes color="purple" />
+      </button>
+      <button className="edit">
+        <FaEdit onClick={() => editFeedback(item)} color="purple" />
       </button>
       <div className="text-display">{item.text}</div>
     </Card>
