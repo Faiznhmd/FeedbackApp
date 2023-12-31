4ebc65d47ca802d2d7c8f5ed1bd3ea1787c894c0
Faiznhmd <fiaznhmd.0624@gmail.com>
2023-12-31 11:19:15 +0530

    uses of usecontext and using click function usecontext

--- src/component/FeedbackList.jsx
+++ src/component/FeedbackList.jsx
@@ -1,7 +1,11 @@
 import { motion, AnimatePresence } from 'framer-motion';
+import { useContext } from 'react';
+import FeedbackContext from '../context/FeedbackContext';
 import FeedbackItem from './FeedbackItem';
 
-const FeedbackList = ({ feedback, handleDelete }) => {
+const FeedbackList = () => {
+  const { feedback } = useContext(FeedbackContext);
+
   if (!feedback || feedback.length === 0) {
     return (
       <p
@@ -26,11 +30,7 @@ const FeedbackList = ({ feedback, handleDelete }) => {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
           >
-            <FeedbackItem
-              key={item.id}
-              item={item}
-              handleDelete={handleDelete}
-            />
+            <FeedbackItem key={item.id} item={item} />
           </motion.div>
         ))}
       </AnimatePresence>
